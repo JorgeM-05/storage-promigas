0c4be789d888f5eb9bbd0889b581b9dfb887b9ed
Jorge Muñoz <51250678+JorgeM-05@users.noreply.github.com>
2023-03-01 12:02:11 -0500

    dsds

--- src/main/java/com/promigas_storage/Service/UploadExcelTest.java
+++ src/main/java/com/promigas_storage/Service/UploadExcelTest.java
@@ -7,23 +7,44 @@ import java.util.List;
 
 public class UploadExcelTest {
     public List<StorageEntity> testStorages(){
-        StorageEntity storage = new StorageEntity();
         List<StorageEntity> listStorage = new ArrayList<>();
+
+        listStorage.add(getStorageOne());
+        listStorage.add(getStorageTwo());
+        return listStorage;
+    }
+    /** simulado con uno de la BD que NO esiste*/
+
+    public StorageEntity getStorageOne(){
+        StorageEntity storage = new StorageEntity();
 
         storage.getCountryEntity().setCod_country("COL");
         storage.getCountryEntity().setUrlFlags("irlll.....");
+        storage.getCountryEntity().setNameContry("");
 
         storage.getOpportunityEntity().setCity("Colombia");
         storage.getOpportunityEntity().setCoordinates("dsdss");
 
+        storage.getCapexEntity().setCapexCop(22.2);
+        storage.getCapexEntity().setCapexCop(22.2);
+        storage.getCapexEntity().setYear("2022");
+
+        return storage;
+    }
+
+    /** simulado con uno de la BD que YA esiste*/
+    public StorageEntity getStorageTwo(){
+        StorageEntity storage = new StorageEntity();
+
+        storage.getCountryEntity().setCod_country("COL");
+        storage.getCountryEntity().setUrlFlags("irlll.....");
+        storage.getCountryEntity().setNameContry("name");
+
+        storage.getOpportunityEntity().setCity("Colombia");
+        storage.getOpportunityEntity().setCoordinates("dsdss");
 
         storage.getCapexEntity().setCapexCop(22.2);
-        
-
-
-
-
-        listStorage.add(storage);
-        return listStorage;
+        storage.getCapexEntity().setCapexCop(22.2);
+        storage.getCapexEntity().setYear("2022");
     }
 }
